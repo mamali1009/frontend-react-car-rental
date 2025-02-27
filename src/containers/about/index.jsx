915dac27d73d808b31452458af6eec23503b18c2
mamali1009 <mohammed.abdulmueedali@cognizant.com>
2025-02-27 20:33:28 +0530

    Add files via upload

--- src/containers/about/index.jsx
+++ src/containers/about/index.jsx
@@ -1,1 +1,16 @@
-
+import React from "react";
+import './style.css';
+const About=()=>{
+    return(
+        <section id="about" className="about">
+        <div className="about_text-wrapper">
+            <h1>
+               About Car Rental App
+            </h1>
+            <br/>
+            <p>Car Rental App is the one stop shop for AI services and capabilities without the need for extensive infrastructure or expertise of scalabiity flexibility codt effectiveness enabling adapting project to quickly deploys and scale AI solutions to meet their specific need and uses open sourcd services which reduce both capex and opex enables adoption of a range of service without cost escalation will provide cost estimate based on the data volume for the service available this will enable user to take informed decision.</p>
+        </div>
+        </section>
+    );
+};
+export default About;
