c47fe426283383824902369d72e75042d64d55cf
mamali1009 <mohammed.abdulmueedali@cognizant.com>
2025-03-03 19:28:03 +0530

    Create index.jsx

---
+++ src/containers/home/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './style.css';
+
+const Home=()=>{
+    return(
+    <section id="home" className="home">
+        <div className="home_text-wrapper">
+            <h1>
+                Car Rental App
+            </h1>
+            <br/>
+            <h5>The Future is Here</h5>
+        </div>
+        </section>
+    );
+};
+export default Home;
